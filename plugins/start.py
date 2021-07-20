8b6fb1ee0b519aae6ba15f36185624f1bd96c668
Zeeenx <87702906+Zeeenx@users.noreply.github.com>
2021-07-20 21:29:53 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,14 +94,14 @@ async def start_command(client: Client, message: Message):
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-    text = "<b>kamu harus bergabung di channel saya untuk menggunakan bot ini !\n\nklik subscribe dibawah ini 👇 dan</b>"
+    text = "<b>kamu harus bergabung di channel saya untuk menggunakan bot ini !\n\nklik subscribe dibawah ini 👇</b>"
     message_text = message.text
     try:
         command, argument = message_text.split()
-        text = text + f" <b>and <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
+        text = text + f" <b>dan <a href='https://t.me/{client.username}?start={argument}'>try again</a></b>"
     except ValueError:
         pass
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("subscribe", url = client.invitelink)]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Subscribe", url = client.invitelink)]])
     await message.reply(
         text = text,
         reply_markup = reply_markup,
