ee7f37f48a0379b93d287107b30e90ca37c79f61
Zeeenx <87702906+Zeeenx@users.noreply.github.com>
2021-08-24 08:16:36 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/DM21SBOT'>Click here</a>\n○ Channel : @Duniamoviee2\n○ Support Group : @DM21SBOT</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://www.google.com/'>Click here</a>\n○ Channel : -\n○ Support Group : -</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
